2fafc5e7fe22a1dc10cce80325abc4e691961954
cos <uT_4011@zsp3zamosc.pl>
2026-03-29 21:57:35 +0200

    zadanie 24.4

--- MauiApp9/NewPage2.xaml.cs
+++ MauiApp9/NewPage2.xaml.cs
@@ -8,8 +8,19 @@ public partial class NewPage2 : ContentPage
 	}
 	async void onExportButtonClicked(object sender, EventArgs e)
 	{
+		string pathToFile = Path.Combine(FileSystem.Current.AppDataDirectory, "notatki_eksport.txt");
+        if(!string.IsNullOrWhiteSpace(FileEditor.Text))
+        {
+		string note = $"[{DateTime.Now:dd.MM.yyyy HH:mm}] {FileEditor.Text}";
+            await File.WriteAllTextAsync(pathToFile, note);
+            await DisplayAlertAsync("Powiadomienie", "Udało się zapisać plik", "Ok");
+        }
+        else
+        {
+            await DisplayAlertAsync("Powiadomienie", "Nie udało się zapisać plik", "Ok");
 
-	}
+        }
+    }
 
     async void onImportButtonClicked(object sender, EventArgs e)
 	{
@@ -20,10 +31,16 @@ public partial class NewPage2 : ContentPage
 			{
 				string fileName = wynik.FileName;
 				string fullPath = wynik.FullPath;
-
+                if(fileName.Contains(".txt"))
+                {
 				await DisplayAlertAsync("Wybrano plik", $"Nazwa:{fileName}", "OK");
 				infoLabel.Text= $"Został wybrany plik {fileName}";
                 FileEditor.Text= await File.ReadAllTextAsync(fullPath);
+                }
+                else
+                {
+                    await DisplayAlertAsync("Powiadomienie", "Aplikacja obsługuje tylko pliki tekstowe", "Ok");
+                }
 
             }
 		}
